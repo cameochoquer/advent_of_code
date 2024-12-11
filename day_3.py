249ba55a0a3bc1475cbedc8c798f023272cf8771
cameochoquer <cameo.choquer@policyinpractice.co.uk>
2024-12-11 19:49:29 +0000

    day3p2

--- day_3.py
+++ day_3.py
@@ -9,10 +9,30 @@ for x in data:
 
 # print(newstring)
 res = 0
-muls = re.findall(r"mul\(\d+,\d+\)", newstring)
-for mul in muls:
-    string_tuple = mul.replace("mul", "")
-    new_tuple = eval(string_tuple)
-    res += new_tuple[0] * new_tuple[1]
+don = re.findall(r"don\'t\(\)", newstring)
+dos = re.findall(r"do\(\)", newstring)
+# muls = re.findall(r"mul\(\d+,\d+\)", newstring)
+new = re.findall(r"mul\(\d+,\d+\)|do\(\)|don\'t\(\)", newstring)
+print(new)
 
+# for mul in muls:
+#     string_tuple = mul.replace("mul", "")
+#     new_tuple = eval(string_tuple)
+#     res += new_tuple[0] * new_tuple[1]
+
+new_list = []
+dont_list = []
+pattern = r"mul\(\d+,\d+\)"
+
+enabled = True
+for value in new:
+    if value == "do()":
+        enabled = True
+    elif value == "don't()":
+        enabled = False
+    elif re.match(pattern, value):
+        if enabled:
+            string_tuple = value.replace("mul", "")
+            new_tuple = eval(string_tuple)
+            res += new_tuple[0] * new_tuple[1]
 print(res)
